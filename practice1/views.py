c55a9ece39e3099f45b79894e11d966ed1d3c882
takano77 <takanotomoyuki123456@gmail.com>
2020-02-24 13:52:53 +0900

    changed fashionssite

--- practice1/views.py
+++ practice1/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'practice1/post_list.html',{})
+def clothes_list(request):
+    return render(request, 'practice1/clothes_list.html',{})
+    
